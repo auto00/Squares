8f6d1157dd6655c795b7bbdd79bf142e070d6ee8
rh <rh@DESKTOP-S5IF0S2>
2018-01-29 02:42:55 +0900

    scommit

--- src/squares/Squares_lib.java
+++ src/squares/Squares_lib.java
@@ -1,33 +1,33 @@
 package squares;
 
-public class Squares_lib implements Squares_if {
+public class Squares_lib implements Squares_if{
 	
-	double x[], y[];
+	double x[],y[];
 	double theta[] = new double[2];
-	double eta = 0.0001;
+	double eta = 0.01;
 	
 	public Squares_lib(double[] x, double[] y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	@Override
 	public double getObject() {
-		//TODO Auto-generated method stub
-		double object = 0;
+		// TODO Auto-generated method stub
+		double object = 0; 
 		for(int i = 0; i < x.length; i++) {
-			object += Math.pow(y[i] - (theta[0] + theta[1] * x[i]), 2);
+			object += Math.pow(y[i] - ( theta[0] + theta[1] * x[i]), 2);
 		}
-		return object / 2;
+		return object/2;
 	}
-	
+
 	@Override
 	public double[] getDx() {
-		//TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 		double object[] = new double[2];
-		for(int i = 0; i < x.length; i++) {
-			object[0] += theta[0] + theta[1] * x[i] - y[i];
-			object[1] += (theta[0] + theta[1] * x[i] - y[i]) * x[i];
+		for(int i = 0; i < x.length; i++){
+		object[0] += theta[0] + theta[1] * x[i] - y[i];
+		object[1] += (theta[0] + theta[1] * x[i] - y[i])*x[i];
 		}
 		theta[0] -= eta * object[0];
 		theta[1] -= eta * object[1];
